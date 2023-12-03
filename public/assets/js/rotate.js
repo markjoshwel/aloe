ed3864d39bd01da92d5b1e8f63ef528ad441e94d
Mark Joshwel <mark@joshwel.co>
2023-12-03 19:09:17 +0000

    miraculously fix a few things

--- public/assets/js/rotate.js
+++
@@ -1,8 +0,0 @@
-const text = document.querySelector("#description-text p");
-text.innerHTML = text.innerText
-    .split("")
-    .map(
-        (char, i) =>
-            `<span style="transform: rotate(${i * 7.4}deg);">${char}</span>`
-    )
-    .join("");
